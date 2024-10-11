e2fac9ae9b208870006de24b1bfc8951815c8564
jyha200 <jyha200@@gmail.com>
2024-10-11 09:50:13 +0000

    Implement LRU with size

--- ethdb/eviction/lru/lru.go
+++ ethdb/eviction/lru/lru.go
@@ -5,16 +5,21 @@ import (
 	"sync"
 )
 
+type LRUEntry struct {
+	list_elem *list.Element
+	size      int
+}
+
 type Eviction struct {
 	lruList list.List
-	elemmap map[string]*list.Element
+	elemmap map[string]LRUEntry
 	mu      sync.Mutex
 }
 
 // Constructor
 func New() *Eviction {
 	ret := Eviction{}
-	ret.elemmap = make(map[string]*list.Element)
+	ret.elemmap = make(map[string]LRUEntry)
 	return &ret
 }
 
@@ -37,29 +42,30 @@ func (e *Eviction) Access(key []byte) bool {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 	strKey := string(key)
-	elem := e.elemmap[strKey]
-	if elem == nil {
+	entry, success := e.elemmap[strKey]
+	if !success {
 		return false
 	}
 
-	e.lruList.MoveToBack(elem)
+	e.lruList.MoveToBack(entry.list_elem)
 	return true
 }
 
-// Return eviction victim  and true, and remove it from policy
-// Return nil and false if there is no element in policy
-func (e *Eviction) Pop() ([]byte, bool) {
+// Return eviction victim, true and the size of kv pair, and remove it from policy
+// Return nil, false, and 0, if there is no element in policy
+func (e *Eviction) Pop() ([]byte, bool, int) {
 	e.mu.Lock()
 	defer e.mu.Unlock()
-	elem := e.lruList.Front()
-	if elem == nil {
-		return nil, false
+	entry := e.lruList.Front()
+	if entry == nil {
+		return nil, false, 0
 	}
 
-	strKey := elem.Value.(string)
-	e.lruList.Remove(elem)
+	strKey := entry.Value.(string)
+	e.lruList.Remove(entry)
+	size := e.elemmap[strKey].size
 	delete(e.elemmap, strKey)
-	return []byte(strKey), true
+	return []byte(strKey), true, size
 }
 
 // Number of keys in policy
@@ -71,33 +77,36 @@ func (e *Eviction) Len() int {
 
 // Add new key to policy and return true
 // return false if key already exists
-func (e *Eviction) Push(key []byte) bool {
+func (e *Eviction) Push(key []byte, size int) bool {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 	strKey := string(key)
-	val := e.elemmap[strKey]
-	if val != nil {
-		e.lruList.MoveToBack(val)
+	entry, success := e.elemmap[strKey]
+
+	if success {
+		entry.size = size
+		e.elemmap[strKey] = entry
+		e.lruList.MoveToBack(entry.list_elem)
 		return false
 	}
-	elem := e.lruList.PushBack(strKey)
-	e.elemmap[strKey] = elem
+	list_elem := e.lruList.PushBack(strKey)
+	e.elemmap[strKey] = LRUEntry{list_elem: list_elem, size: size}
 	return true
 
 }
 
 // Remove key from policy and return true
 // Return false if key does not exist
-func (e *Eviction) Delete(key []byte) bool {
+func (e *Eviction) Delete(key []byte) (bool, int) {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 	strKey := string(key)
-	elem := e.elemmap[strKey]
-	if elem == nil {
-		return false
+	entry, success := e.elemmap[strKey]
+	if !success {
+		return false, 0
 	}
-
-	e.lruList.Remove(elem)
+	size := entry.size
+	e.lruList.Remove(entry.list_elem)
 	delete(e.elemmap, strKey)
-	return true
+	return true, size
 }
